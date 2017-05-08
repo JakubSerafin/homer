e6e14986e0c834d70780318f9849b68143a014a6
Thaven <sirthaven@gmail.com>
2017-05-08 09:32:50 +0200

    tests fixes

--- homerApi.tests/InterpolationServiceTests.cs
+++ homerApi.tests/InterpolationServiceTests.cs
@@ -13,11 +13,11 @@ namespace homerApi.Tests
         public void GetInterpolation_OneDayOneRecord_ReturnMeasurmentValue()
         {
             var service = new InterpolationService();
-            var data = new List<Measurment>(){new Measurment{Date = DateTime.Today,Value = 10}};
+            var data = new List<Measurment>() { new Measurment { Date = DateTime.Today, Value = 10 } };
 
-            var interpolations =  service.GetInterpolationForInterval(DateTime.Today, DateTime.Today, TimeSpan.FromDays(1), data);
+            var interpolations = service.GetInterpolationForInterval(DateTime.Today, DateTime.Today, TimeSpan.FromDays(1), data);
             var enumerable = interpolations as Interpolation[] ?? interpolations.ToArray();
-            Assert.Equal(1,enumerable.Count());
+            Assert.Equal(1, enumerable.Count());
             Assert.Equal(10, enumerable.First().Value);
         }
 
@@ -35,10 +35,10 @@ namespace homerApi.Tests
             var interpolations = service.GetInterpolationForInterval(DateTime.Today, DateTime.Today.AddDays(2), TimeSpan.FromDays(1), data);
             var enumerable = interpolations as Interpolation[] ?? interpolations.ToArray();
             Assert.Collection(enumerable,
-                _=>Assert.Equal(10,_.Value),
-                _=>Assert.Equal(15,_.Value),
-                _=>Assert.Equal(20,_.Value)
-                );
+                _ => Assert.Equal(10, _.Value),
+                _ => Assert.Equal(15, _.Value),
+                _ => Assert.Equal(20, _.Value)
+            );
         }
     }
 }
