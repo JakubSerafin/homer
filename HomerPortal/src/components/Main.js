a9dc773d821bf4fb0086cfeba14905a54c111f73
Thaven <sirthaven@gmail.com>
2017-05-09 22:50:14 +0200

    connected to webapi

--- HomerPortal/src/components/Main.js
+++ HomerPortal/src/components/Main.js
@@ -4,16 +4,29 @@ require('styles/App.css');
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
-let counters = ['raz', 'dwa', 'trzy']
 
 class AppComponent extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = { items: [] };
+  }
+  
+  componentDidMount()
+  {
+    fetch('http://homerapp.azurewebsites.net/api/Measurements')
+    .then(result=>result.json())
+    .then(items=>this.setState({items}))
+  }
+
   render() {
     return (
       <div className="index">
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-        <div>I did it already :)</div>
-        {counters.map((obj,i)=>{return <div>{obj}</div>})} 
+        <ul>
+              {this.state.items.map(item=><li key={item.id}>date: {item.date}, type:{item.measurmentType}, value: {item.value}</li>)}
+        </ul>
       </div>
     );
   }
